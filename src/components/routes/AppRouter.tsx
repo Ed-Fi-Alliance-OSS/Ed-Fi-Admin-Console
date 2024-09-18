2c92ec727af7beb0910a575e4545b3870b10c9e9
Juan Agudelo <jagudelo@wearegap.com>
2024-09-18 11:15:01 -0500

    Fix a reference in AppRouter

--- src/components/routes/AppRouter.tsx
+++ src/components/routes/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route } from "react-router"
+import { Navigate, Route } from "react-router-dom"
 import { Routes } from "react-router-dom"
 import routes from "../../core/routes"
 import HomePage from "../pages/HomePage"
