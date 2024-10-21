ed4ef60aea11f325d57e8d84687e355da6db8f1c
Danny Fernandez A <dfernandeza@growthaccelerationpartners.com>
2024-10-21 11:22:00 -0600

    Fix e2e tests

--- e2e/instance-page/summary-tab.spec.ts
+++ e2e/instance-page/summary-tab.spec.ts
@@ -1,15 +1,16 @@
-
 import { test, expect } from "@playwright/test"
 import { routes } from "../core/routes"
 
 test.beforeEach(async ({ page }) => {
-    await page.goto(routes.instance)
-    await page.waitForURL(routes.instance)
-    await page.waitForLoadState('networkidle')
+    await page.goto(routes.home);
+    await page.waitForURL(routes.home);
+    await page.waitForLoadState('networkidle');
+    await page.getByText('2023 - 2024').click();
+    await page.waitForURL(routes.instance);
 })
 
-
 test("Should show instance data if Onboarding Wizard has been finished", async ({ page }) => {
+    await page.getByRole("tab", { name: "Summary" }).waitFor()
     await page.getByRole("tab", { name: "Summary" }).click()
 
     await expect(page.getByText("Ed-Fi Base URL")).toBeVisible({ timeout: 15000 })
@@ -18,4 +19,3 @@ test("Should show instance data if Onboarding Wizard has been finished", async (
     await expect(page.getByText("Ed-Fi Status")).toBeVisible()
     await expect(page.getByText("Hosting")).toBeVisible()
 })
-    
