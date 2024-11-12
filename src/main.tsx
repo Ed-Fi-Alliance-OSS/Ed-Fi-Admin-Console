7f2a608693b60fc6bda83d0276f0bf772dcec6dc
Hammad Asif <mrhammadasif@gmail.com>
2024-11-12 05:06:41 +0500

    Fix the Dockerfile for deployment (#30)
    * add mode product to Dockerfile
    
    * Fix the App to main
    
    * remove extra config file

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
-/*
- <React.StrictMode>
-  </React.StrictMode>
-
-*/
 console.log(`basePath is ${import.meta.env.BASE_URL }`)
 fetch(`${import.meta.env.BASE_URL || './'}config.json`).then(resp => resp.json()).then(config => {
-  // console.log(`basePath is ${config.app.basePath}`)
-
   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <BrowserRouter basename={config.app.basePath}>
       <App appConfig={config} />
