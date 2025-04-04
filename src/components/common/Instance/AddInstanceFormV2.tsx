817eed4b93399ed0e834dfa137699ec99633c85e
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2025-04-04 09:13:41 -0600

    Update form to use formik

--- src/components/common/Instance/AddInstanceFormV2.tsx
+++ src/components/common/Instance/AddInstanceFormV2.tsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
+import {
+  Form,
+  Formik
+} from 'formik'
 import { Button, Flex, Input, Box, Text } from "@chakra-ui/react";
-import { useApiService, useConfig } from "@edfi/admin-console-shared-sdk";
-import { usePluginContext } from "../../../plugins/BasePlugin";
+import { CustomFormHeader, CustomSelect, useApiService, useConfig } from "@edfi/admin-console-shared-sdk";
+import * as Yup from 'yup'
+import AppInput from "../../AppInput";
 
 
 export interface AddInstanceFormProps {
@@ -23,13 +28,18 @@ const AddInstanceFormV2: React.FC<AddInstanceFormProps> = ({
   odsInstanceContexts,
   odsInstanceDerivatives,
   onSaveChanges,
-})=> {
+}) => {
   const [formData, setFormData] = useState<AddInstanceFormProps>({
     name,
     instanceType,
     odsInstanceContexts,
     odsInstanceDerivatives,
-    onSaveChanges: () => {},
+    onSaveChanges
+  });
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required('Name is required'),
+    instanceType: Yup.string().required('Instance Type is required'),
   });
 
   const handleChange = (e) => {
@@ -40,161 +50,170 @@ const AddInstanceFormV2: React.FC<AddInstanceFormProps> = ({
     }));
   };
 
-  const handleAddContext = () => {
-    setFormData((prevData) => ({
-      ...prevData,
-      odsInstanceContexts: [
-        ...prevData.odsInstanceContexts,
-        { contextKey: "", contextValue: "" },
-      ],
-    }));
-  };
-
-  const handleContextChange = (index, field, value) => {
-    const updatedContexts = [...formData.odsInstanceContexts];
-    updatedContexts[index][field] = value;
-    setFormData((prevData) => ({
-      ...prevData,
-      odsInstanceContexts: updatedContexts,
-    }));
-  };
-
-  const handleRemoveContext = (index) => {
-    const updatedContexts = formData.odsInstanceContexts.filter(
-      (_, i) => i !== index
-    );
-    setFormData((prevData) => ({
-      ...prevData,
-      odsInstanceContexts: updatedContexts,
-    }));
-  };
-
-  const handleAddDerivative = () => {
-    setFormData((prevData) => ({
-      ...prevData,
-      odsInstanceDerivatives: [
-        ...prevData.odsInstanceDerivatives,
-        { derivativeType: "" },
-      ],
-    }));
-  };
-
-  const handleDerivativeChange = (index, field, value) => {
-    const updatedDerivatives = [...formData.odsInstanceDerivatives];
-    updatedDerivatives[index][field] = value;
-    setFormData((prevData) => ({
-      ...prevData,
-      odsInstanceDerivatives: updatedDerivatives,
-    }));
-  };
-
-  const handleRemoveDerivative = (index) => {
-    const updatedDerivatives = formData.odsInstanceDerivatives.filter(
-      (_, i) => i !== index
-    );
-    setFormData((prevData) => ({
-      ...prevData,
-      odsInstanceDerivatives: updatedDerivatives,
-    }));
-  };
-
-  const { config } = useConfig();
-  const { functionalities } = usePluginContext();
-  const apiService = functionalities.ApiService?.(config, useApiService);
-  
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSaveChanges(formData); 
-  };
   return (
-    <form onSubmit={handleSubmit} className="add-instance-form">
-      <Box mb="16px">
-        <Text fontWeight="bold">Name:</Text>
-        <Input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-        />
-      </Box>
-      <Box mb="16px">
-        <Text fontWeight="bold">Instance Type:</Text>
-        <Input
-          type="text"
-          name="instanceType"
-          value={formData.instanceType}
-          onChange={handleChange}
-        />
-      </Box>
-
-      <Box mb="16px">
-        <Text fontWeight="bold">ODS Instance Contexts:</Text>
-        {formData.odsInstanceContexts.map((context, index) => (
-          <Flex key={index} mb="8px" alignItems="center">
-            <Input
-              placeholder="Context Key"
-              value={context.contextKey}
-              onChange={(e) =>
-                handleContextChange(index, "contextKey", e.target.value)
-              }
-              mr="8px"
-            />
-            <Input
-              placeholder="Context Value"
-              value={context.contextValue}
-              onChange={(e) =>
-                handleContextChange(index, "contextValue", e.target.value)
-              }
-              mr="8px"
-            />
-            <Button
-              colorScheme="red"
-              onClick={() => handleRemoveContext(index)}
+    <Flex
+      flexDir='column'
+      w='full'
+    >
+      <Formik
+        validationSchema={validationSchema}
+        initialValues={{
+          name: formData.name,
+          instanceType: formData.instanceType,
+          odsInstanceContexts: formData.odsInstanceContexts,
+          odsInstanceDerivatives: formData.odsInstanceDerivatives,
+          onSaveChanges,
+        }}
+        onSubmit={onSaveChanges}>
+        {({ values, setFieldValue }) => (
+          <>
+            <CustomFormHeader text="Instance Details" />
+            <Flex
+              flexDir='column'
+              ml='10px'
+              mt='16px'
+              w='full'
             >
-              Remove
-            </Button>
-          </Flex>
-        ))}
-        <Button colorScheme="blue" onClick={handleAddContext}>
-          Add Context
-        </Button>
-      </Box>
+              <Form>
+                <AppInput
+                  required
+                  fieldName='name'
+                  label='Name'
+                  onChange={handleChange}
+                />
+                <AppInput
+                  required
+                  fieldName='instanceType'
+                  label='Instance Type'
+                  onChange={handleChange}
+                />
+                <Text fontFamily='Poppins'
+                  fontSize='14px'
+                  fontWeight='700'
+                  lineHeight='20px'>ODS Instance Contexts:</Text>
+                {values.odsInstanceContexts.map((context, index) => (
+                  <Flex key={index} mb="8px" alignItems="center">
+                    <Input
+                      placeholder="Context Key"
+                      value={context.contextKey}
+                      onChange={(e) =>
+                        setFieldValue(
+                          `odsInstanceContexts[${index}].contextKey`,
+                          e.target.value
+                        )
+                      }
+                      required
+                      size='xs'
+                    />
+                    <Input
+                      placeholder="Context Value"
+                      value={context.contextValue}
+                      onChange={(e) =>
+                        setFieldValue(
+                          `odsInstanceContexts[${index}].contextValue`,
+                          e.target.value
+                        )
+                      }
+                      required
+                      size='xs'
+                    />
+                    <Button
+                      colorScheme="red"
+                      onClick={() => {
+                        const updatedContexts = values.odsInstanceContexts.filter(
+                          (_, i) => i !== index
+                        );
+                        setFieldValue("odsInstanceContexts", updatedContexts);
+                      }}
+                    >
+                      Remove
+                    </Button>
+                  </Flex>
+                ))}
+                <Button colorScheme="blue" onClick={() =>
+                  setFieldValue("odsInstanceContexts", [
+                    ...values.odsInstanceContexts,
+                    { contextKey: "", contextValue: "" },
+                  ])
+                }>
+                  Add Context
+                </Button>
+                <Box mb="16px">
+                  <Text fontFamily='Poppins'
+                    fontSize='14px'
+                    fontWeight='700'
+                    lineHeight='20px'>ODS Instance Derivatives:</Text>
+                  {values.odsInstanceDerivatives.map((derivative, index) => (
+                    <Flex key={index} mb="8px" flexDir="column">
+                      <Flex alignItems="center">
+                        <CustomSelect
+                          options={[
+                            { text: "ReadReplica", value: "ReadReplica" },
+                            { text: "Snapshot", value: "Snapshot" },
+                          ]}
+                          disabled={false}
+                          id={`derivative-${index}`}
+                          value={derivative.derivativeType}
+                          onChange={(e) => {
+                            const selectedValue = e.target.value;
+                            setFieldValue(
+                              `odsInstanceDerivatives[${index}].derivativeType`,
+                              selectedValue
+                            );
+                          }}
+                          placeholder="Select an option"
+                          size="xs"
+                          required
+                        />
+                        <Button
+                          colorScheme="red"
+                          ml="8px"
+                          onClick={() => {
+                            const updatedDerivatives =
+                              values.odsInstanceDerivatives.filter(
+                                (_, i) => i !== index
+                              );
+                            setFieldValue("odsInstanceDerivatives", updatedDerivatives);
+                          }}
+                        >
+                          Remove
+                        </Button>
+                      </Flex>
+                      {derivative.derivativeType === "" && (
+                        <Text color="red.500" fontSize="xs" mt="4px">
+                          Derivative Type is required
+                        </Text>
+                      )}
+                    </Flex>
 
-      <Box mb="16px">
-        <Text fontWeight="bold">ODS Instance Derivatives:</Text>
-        {formData.odsInstanceDerivatives.map((derivative, index) => (
-          <Flex key={index} mb="8px" alignItems="center">
-            <Input
-              placeholder="Derivative Type"
-              value={derivative.derivativeType}
-              onChange={(e) =>
-                handleDerivativeChange(index, "derivativeType", e.target.value)
-              }
-              mr="8px"
-            />
-            <Button
-              colorScheme="red"
-              onClick={() => handleRemoveDerivative(index)}
-            >
-              Remove
-            </Button>
-          </Flex>
-        ))}
-        <Button colorScheme="blue" onClick={handleAddDerivative}>
-          Add Derivative
-        </Button>
-      </Box>
+                  ))}
+                  <Button colorScheme="blue" onClick={() => {
+                    setFieldValue("odsInstanceDerivatives", [
+                      ...values.odsInstanceDerivatives,
+                      { derivativeType: "" },
+                    ])
+                  }
+                  }>
+                    Add Derivative
+                  </Button>
+                </Box>
 
-      <Button
-        mt='32px'
-        size='lg'
-        type='submit'
-        variant='primaryBlue600'
-        w='250px'
-        >
-        Create Instance
-     </Button>
-    </form>
+                <Button
+                  mt='32px'
+                  size='lg'
+                  type='submit'
+                  variant='primaryBlue600'
+                  w='250px'
+                >
+                  Create Instance
+                </Button>
+              </Form>
+            </Flex>
+
+          </>
+        )}
+      </Formik>
+    </Flex>
   );
 };
 
