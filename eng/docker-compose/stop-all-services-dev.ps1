2e8f4342335600e2aec543e21e88a32de3057117
Juan Agudelo <jagudelo@wearegap.com>
2025-04-14 10:13:32 -0500

    Fix script to stop services

--- eng/docker-compose/stop-all-services-dev.ps1
+++ eng/docker-compose/stop-all-services-dev.ps1
@@ -14,6 +14,9 @@ $composeFilePath = Join-Path $PSScriptRoot 'compose-keycloak-dev.yml'
 $composeOds = Join-Path $PSScriptRoot  'compose-ods-multi-tenant-dev.yml'
 $composeLocalAdminApi = Join-Path $PSScriptRoot  'compose-adminapi-dev.yml'
 $composeLocalAdminConsole = Join-Path $PSScriptRoot  'compose-adminconsole-local-dev.yml'
+$composeHealthCheckWorker = Join-Path $PSScriptRoot compose-Health-Check-Worker-Process.yml
+$composeInstanceManagementWorker = Join-Path $PSScriptRoot compose-Instance-Management-Worker-Process.yml
+
 $envFilePath = Join-Path $PSScriptRoot '.env'
 
 $params = @(
@@ -25,10 +28,10 @@ $params = @(
     "--remove-orphans"
 )
 if ($v) {
-    $params = $params[0..1] + "-f" + $composeLocalAdminConsole + "-f" + $composeLocalAdminApi + "-f" + $composeOds + $params[2..9] + "-v"
+    $params = $params[0..1] + "-f" + $composeLocalAdminConsole + "-f" + $composeLocalAdminApi + "-f" + $composeOds + "-f" + $composeHealthCheckWorker + "-f" + $composeInstanceManagementWorker + $params[2..9] + "-v"
 }
 else {
-    $params = $params[0..1] + "-f" + $composeLocalAdminConsole + "-f" + $composeLocalAdminApi + "-f" + $composeOds + $params[2..9] 
+    $params = $params[0..1] + "-f" + $composeLocalAdminConsole + "-f" + $composeLocalAdminApi + "-f" + $composeOds + "-f" + $composeHealthCheckWorker + "-f" + $composeInstanceManagementWorker + $params[2..9] 
 }
 
 & docker compose $params
