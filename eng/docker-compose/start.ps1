88a65c0893586b284d98aa474ba4800917118183
Juan Agudelo <jagudelo@growthaccelerationpartners.com>
2025-05-28 22:38:20 -0500

    Add some adjustments to the compose files. Include a possible solution to run docker locally. Improvements in documentation based on changes

--- eng/docker-compose/start.ps1
+++ eng/docker-compose/start.ps1
@@ -20,8 +20,8 @@ $composeOds = Join-Path $PSScriptRoot compose-ods-multi-tenant-dev.yml
 $composeLocalAdminApi = Join-Path $PSScriptRoot compose-adminapi-dev.yml
 $composePublishedAdminConsole = Join-Path $PSScriptRoot compose-adminconsole-published.yml
 
-$composeHealthCheckWorker = Join-Path $PSScriptRoot compose-Health-Check-Worker-Process.yml
-$composeInstanceManagementWorker = Join-Path $PSScriptRoot compose-Instance-Management-Worker-Process.yml
+$composeHealthCheckWorker = Join-Path $PSScriptRoot compose-healthcheck.yml
+$composeInstanceManagementWorker = Join-Path $PSScriptRoot compose-instance-management.yml
 
 $envFilePath = Join-Path $PSScriptRoot .env
 
